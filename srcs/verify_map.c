4dfb6a59e8bd27d6b8caa9f65acb29b55fafd40f
flotapponnier <florent.tapponnier@gmail.com>
2024-11-01 10:14:03 +0100

    valid map more clear + floodfill

--- srcs/verify_map.c
+++ srcs/verify_map.c
@@ -1,6 +1,6 @@
 #include "so_long.h"
 
-char **ft_get_map(int fd)
+char **ft_get_map (int fd)
 {
     char **result;
     int i;
@@ -10,7 +10,6 @@ char **ft_get_map(int fd)
     result = malloc(sizeof(char *) * (MAX_LINES + 1));
     if (result == NULL)
         return (NULL);
-
     i = 0;
     while (i < MAX_LINES)
     {
@@ -20,10 +19,10 @@ char **ft_get_map(int fd)
         i++;
     }
     result[i] = NULL;
-
     valid_map = ft_verif_map(result);
     if (!valid_map)
     {
+		j = 0;
         while (j < i)
         {
             free(result[j]);
@@ -32,11 +31,12 @@ char **ft_get_map(int fd)
         free(result);
         return (NULL);
     }
-
-    for (j = 0; j < i; j++)
+	j = 0;
+    while(j < i)
     {
         printf("%s", result[j]);
         free(result[j]);
+		j++;
     }
     
     free(result);
@@ -97,27 +97,111 @@ int	ft_is_valid_caracter(char **str)
 	return (1);
 }
 
+
+int ft_find_position_p(char **map, int *row, int *col)
+{
+	while(map[*col])
+	{
+		while(map[*col][*row])
+		{
+			if(map[*col][*row] == 'P')
+			{
+				return (1);			
+			}
+			(*row)++;
+		}
+		(*row) = 0;
+		(*col)++;
+	}
+	return (0);
+}
+
+char **ft_map_cpy(char **map)
+{
+	int		i;
+	char	**map_cpy;
+	
+	i = 0;
+	while(map[i])
+		i++;
+	map_cpy = malloc(sizeof(char *) * (i + 1));
+	if(!map_cpy)
+		return (NULL);
+	i = 0;
+	while(map[i])
+	{
+		map_cpy[i] = ft_strdup(map[i]);
+		if(!map_cpy[i])
+		{
+			while(i-- > 0)
+			{
+				free(map_cpy[i]);
+			}
+			free(map_cpy);
+			return (NULL);
+		}
+		i++;
+	}
+	map_cpy[i] = NULL;
+	return (map_cpy);
+}
+
+int ft_is_flood_valid(char **map, int col, int row)
+{
+	if(map[col][row] == 'E' || map[col][row] == '0' || map[col][row] == 'C' )
+		return (1);
+	return (0);
+}
+
+char **ft_flood_map(char **map, int col, int row)
+{
+	if(!ft_is_flood_valid(map, col, row)
+		return (map);
+	map[col][row] = 'v';
+	if(ft_is_flood_valid(map, col + 1 , row))
+		ft_flood_map(map, col + 1, row);
+	if(ft_is_flood_valid(map, col - 1 , row))
+		ft_flood_map(map, col - 1, row);
+	if(ft_is_flood_valid(map, col, row + 1))
+		ft_flood_map(map, col, row - 1);
+	if(ft_is_flood_valid(map, col, row + 1))
+		ft_flood_map(map, col, row + 1);
+	return (map);
+}
+
 int	ft_verif_map(char **result)
 {
 	int i;
+	int col;
+	int row;
 	size_t count_row;
-	
+	char **map;
+
+	map = ft_map_cpy(result);
 	i = 0;
-	count_row = ft_strlen(result[i]);
-	if (!ft_is_valid_caracter(result)) 
-		{
-			printf("Error invalid caracter \n");
+	col = 0;
+	row = 0;
+	count_row = ft_strlen(map[i]);
+	if (!ft_is_valid_caracter(map)) 
+		{ printf("Error invalid caracter \n");
 			return (0);
 		}
-	while(result[i])	
+	while(map[i])	
 	{
-		if (!ft_is_rectangle(result[i], count_row))
+		if (!ft_is_rectangle(map[i], count_row))
 		{
 			printf("Error : width problem \n");
 			return (0);
 		}
-		
 		i++;
 	}
+	// flood fill 
+	if (!ft_find_position_p(map, &col, &row))
+	{
+		printf("Position P not found");
+		return (0);
+	}
+	printf("Position P found at col : %d row : %d \n", col, row);
+	map = ft_flood_map(map, col, row);
 	return (1);
 }
